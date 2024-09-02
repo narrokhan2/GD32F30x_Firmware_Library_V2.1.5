0b0e19c9525679aa028a7ba4b12dee8279ae0932
narrokhan2 <narrokhan@gmail.com>
2024-09-02 16:45:10 +0800

    Initial commit

---
+++ GD32F30x_Firmware_Library_V2.1.5/Examples/SPI/SPI_master_slave_fullduplex_dma/Objects/main.d
@@ -0,0 +1,8 @@
+.\objects\main.o: main.c
+.\objects\main.o: ..\..\..\Firmware\CMSIS\GD\GD32F30x\Include\gd32f30x.h
+.\objects\main.o: ..\..\..\Firmware\CMSIS\core_cm4.h
+.\objects\main.o: C:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
+.\objects\main.o: ..\..\..\Firmware\CMSIS\core_cmInstr.h
+.\objects\main.o: ..\..\..\Firmware\CMSIS\core_cmFunc.h
+.\objects\main.o: ..\..\..\Firmware\CMSIS\core_cm4_simd.h
+.\objects\main.o: ..\..\..\Firmware\CMSIS\GD\GD32F30x\Include\system_gd32f30x.h
---
+++ GD32F30x_Firmware_Library_V2.1.5/Template/Keil_project/output/startup_gd32f30x_hd_1.d
@@ -0,0 +1,1 @@
+.\output\startup_gd32f30x_hd_1.o: RTE/Device/GD32F303RC/startup_gd32f30x_hd.s
